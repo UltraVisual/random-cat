0f62cd23bec91ab0d5782c3a843839d840419cf0
Shane Johnson <shanej@ultravisual.co.uk>
2018-11-23 16:08:22 +0000

    tidy up

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const { send } = require('micro');
 const axios = require("axios");
-const API_KEY = process.env.API_KEY;
-const TAG = "cat";
 const API_URL = `https://api.thecatapi.com/v1/images/search?size=full`;
 
 const getCatGif = async () => {
   const response = await axios.get(API_URL, {
     headers: { Accept: "application/json" }
   });
-
-  const { log } = console;
-  log('SHANE:', response);
 
   return response.data[0].url;
 };
